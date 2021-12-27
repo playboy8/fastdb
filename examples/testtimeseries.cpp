ecab0eb966a2a00a1a46e2d08461924a0226fa29
louis <const_ryte@163.com>
2021-12-27 13:44:38 +0800

    无盘模式配置

--- examples/testtimeseries.cpp
+++ examples/testtimeseries.cpp
@@ -47,8 +47,8 @@ REGISTER_TEMPLATE(DailyBlock);
 REGISTER(Stock);
 
 inline int random(unsigned mod) { return rand() % mod; }
-inline float fmax(float x, float y) { return x > y ? x : y; }
-inline float fmin(float x, float y) { return x < y ? x : y; }
+//inline float fmax(float x, float y) { return x > y ? x : y; }
+//inline float fmin(float x, float y) { return x < y ? x : y; }
 
 int main(int argc, char* argv[])
 {
